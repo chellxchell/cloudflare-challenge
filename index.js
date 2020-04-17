e69a9b835fabe26f420d17c4c5e3c4a674df50e7
Chelly Compendio <rochellecompendio2021@u.northwestern.edu>
2020-04-16 22:22:43 -0500

    switches between two screens

--- index.js
+++ index.js
@@ -6,33 +6,9 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 
-// console.log(response.headers.get('Content-Type'));
-// console.log(JSON.parse(JSON.stringify(response)).url)
-// return fetch('https://cfw-takehome.developers.workers.dev/api/variants').then(function(response) {
-//   return response;
-// })
-
-let variants;
-
-async function handleRequest(request) {
-  // make fetch request to URL
-  let urls = reqURLS('https://cfw-takehome.developers.workers.dev/api/variants');
-  return urls;
-  // return fetch('https://cfw-takehome.developers.workers.dev/variants/1');
-}
-
-async function reqURLS(request){
-  return fetch(request)
-    .then((resp) => resp.json())
-    .then((resp) => new Response(resp.variants))
-}
-
-// out of two urls, pick one with a 50/50 chance
-async function pickURL(){
-  if (Math.random() >= 0.5){
-    return 0
-  }
-  else{
-    return 1
-  }
+async function handleRequest() {
+  let num = (Math.random() >= 0.5) ? 0 : 1; // pick 0 or 1 with 50/50 chance
+  return fetch('https://cfw-takehome.developers.workers.dev/api/variants') // request URLS from API
+    .then((resp) => resp.json()) // parse response as JSON
+    .then((resp) => fetch(resp.variants[num].toString())) // pick one of the URLS
 }
